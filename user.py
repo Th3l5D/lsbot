7e6e6a0d2c8cd69cb75c0919ca0791979362a0da
The lsd <thelsd687@gmail.com>
2017-10-15 13:28:23 +0200

    ooops, forgot the user class

---
+++ user.py
@@ -0,0 +1,11 @@
+
+
+class user(object):
+    """docstring for user"""
+    def __init__(self, nick):
+        self.nickame = nick
+        self.identified = None
+        self.secure = None
+        self.channels = None
+        self.vhost = None
+        
